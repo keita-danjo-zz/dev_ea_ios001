e0cabe4e1a67762f913fc46c3a61affb409c5908
檀上恵太 <ke-danjo@global-mobility-service.com>
2016-10-16 21:14:43 +0900

    iosの実装

---
+++ dev_apns/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev_apns
+//
+//  Created by 檀上恵太 on 2016/10/16.
+//  Copyright © 2016年 檀上恵太. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
